831107b69296c68fd65c59da97df394a850d3844
Glitch (mspretro-api) <none>
2023-06-07 19:43:45 +0000

    Fix a bug in GetHighscore and performance improvements for GetHighscoreLook

--- Services/GetHighscoreLook.js
+++ Services/GetHighscoreLook.js
@@ -1,5 +1,5 @@
-const { userModel, friendModel, lookModel } = require("../Utils/Schemas.js");
-const { buildXML, buildPage, formatDate } = require("../Utils/Util.js");
+const { friendModel, lookModel } = require("../Utils/Schemas.js");
+const { buildXML, formatDate } = require("../Utils/Util.js");
 
 exports.data = {
   SOAPAction: "GetHighscoreLook",
@@ -9,94 +9,221 @@ exports.data = {
 
 exports.run = async (request, ActorId) => { 
   let lookArray = [ ];
-  let totalRecords;
+  let lookData;
   
   if (request.forFriends) {
-    const friends1 = await friendModel.find({ RequesterId: ActorId, Status: 1 });
-    const friends2 = await friendModel.find({ ReceiverId: ActorId, Status: 1 });
+    const pipeline = [
+      {
+        $match: {
+          $or: [
+            { RequesterId: ActorId },
+            { ReceiverId: ActorId }
+          ], 
+          Status: 1
+        }
+      },
+      {
+        $lookup: {
+          from: "looks", 
+          let: {
+            actorId: {
+              $cond: [
+                {
+                  $eq: [ "$RequesterId", ActorId ]
+                }, "$ReceiverId", "$RequesterId"
+              ]
+            }
+          }, 
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $or: [
+                    {
+                      $eq: [ "$$actorId", "$ActorId" ]
+                    }
+                  ]
+                }
+              }
+            },
+            {
+              $lookup: {
+                from: "users",
+                localField: "ActorId",
+                foreignField: "ActorId",
+                as: "user"
+              }
+            },
+            {
+              $unwind: "$user"
+            }
+          ],
+          as: "looks"
+        }
+      },
+      {
+        $unwind: "$looks"
+      },
+      {
+        $replaceRoot: {
+          newRoot: "$looks"
+        }
+      },
+      {
+        $unionWith: {
+          coll: "looks", 
+          pipeline: [
+            {
+              $match: {
+                ActorId: ActorId
+              }
+            },
+            {
+              $lookup: {
+                from: "users",
+                localField: "ActorId",
+                foreignField: "ActorId",
+                as: "user"
+              }
+            },
+            {
+              $unwind: "$user"
+            }
+          ]
+        }
+      },
+      {
+        $match: {
+          ActorId: { $ne: 1 },
+          State: 0
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          LookId: "$LookId",
+          ActorId: "$ActorId",
+          Created: "$Created", 
+          Headline: "$Headline", 
+          LookData: "$LookData", 
+          Likes: {
+            $size: "$Likes"
+          },
+          Sells: {
+            $size: "$Sells"
+          },
+          ActorName: "$user.Name"
+        }
+      },
+      {
+        $sort: { Likes: -1 }
+      },
+      {
+        $facet: {
+          looks: [
+            { $skip: request.pageindex * 5 },
+            { $limit: 5 }
+          ], 
+          totalCount: [{ $count: "count" }]
+        }
+      },
+      {
+        $project: {
+          looks: 1, 
+          totalCount: {
+            $arrayElemAt: [ "$totalCount.count", 0 ]
+          }
+        }
+      }
+    ];
     
-    let FriendData = [ ];
-    FriendData.push(ActorId);
-    
-    for(let friend of friends1) {
-      FriendData.push(friend.ReceiverId);
-    };
-  
-    for(let friend of friends2) {
-      FriendData.push(friend.RequesterId);
-    };
+    let sortStage = pipeline.find(stage => stage.hasOwnProperty("$sort"));
+    if (request.orderBy === "SELLS") sortStage.$sort = { Sells: -1 };
     
-    for(let friend of FriendData) {
-      if (friend == 1) continue;
-      
-      const looks = await lookModel.find({ ActorId: friend, State: 0 });
-      if (!looks) continue;
-      
-      const user = await userModel.findOne({ ActorId: friend });
-      
-      for (let look of looks) {
-        lookArray.push({
-          LookId: look.LookId,
-          ActorId: look.ActorId,
-          Created: formatDate(look.Created),
-          Headline: look.Headline,
-          LookData: look.LookData,
-          Likes: look.Likes.length,
-          Sells: look.Sells.length,
-          ActorName: user.Name
-        });
-      };
-    };
+    lookData = await friendModel.aggregate(pipeline);
+  } else {
+    const pipeline = [
+      {
+        $match: {
+          ActorId: { $ne: 1 },
+          State: 0
+        }
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "ActorId",
+          foreignField: "ActorId",
+          as: "user"
+        }
+      },
+      {
+        $unwind: "$user"
+      },
+      {
+        $project: {
+          _id: 0,
+          LookId: "$LookId",
+          ActorId: "$ActorId",
+          Created: "$Created", 
+          Headline: "$Headline", 
+          LookData: "$LookData", 
+          Likes: {
+            $size: "$Likes"
+          },
+          Sells: {
+            $size: "$Sells"
+          },
+          ActorName: "$user.Name"
+        }
+      },
+      {
+        $sort: { Likes: -1 }
+      },
+      {
+        $facet: {
+          looks: [
+            { $skip: request.pageindex * 5 },
+            { $limit: 5 }
+          ], 
+          totalCount: [{ $count: "count" }]
+        }
+      },
+      {
+        $project: {
+          looks: 1, 
+          totalCount: {
+            $arrayElemAt: [ "$totalCount.count", 0 ]
+          }
+        }
+      }
+    ];
     
-    if (request.orderBy === "LIKES") lookArray.sort(function (a, b) {
-      return b.Likes - a.Likes;
+    let sortStage = pipeline.find(stage => stage.hasOwnProperty("$sort"));
+    if (request.orderBy === "SELLS") sortStage.$sort = { Sells: -1 };
+    
+    lookData = await lookModel.aggregate(pipeline);
+  }
+  
+  lookData = lookData[0];
+  
+  let leaderboardArray = [ ];    
+
+  for (let look of lookData.looks) {
+    lookArray.push({
+      LookId: look.LookId,
+      ActorId: look.ActorId,
+      Created: formatDate(look.Created),
+      Headline: look.Headline,
+      LookData: look.LookData,
+      Likes: look.Likes,
+      Sells: look.Sells,
+      ActorName: look.ActorName
     });
-    else lookArray.sort(function (a, b) {
-      return b.Sells - a.Sells;
-    });
-    
-    totalRecords = lookArray.length;
-    lookArray = buildPage(request.pageindex, 5, lookArray);
-  } else {    
-    let looks;
-    
-    if (request.orderBy === "LIKES") looks = await lookModel.aggregate([
-      { $match: { ActorId: { $ne: 1 }, State: 0 } },
-      { $addFields: { len: { $size: "$Likes" }}},
-      { $sort: { len: -1 }},
-      { $skip: request.pageindex * 5 },
-      { $limit: 5 }
-    ]);
-    else looks = await lookModel.aggregate([
-      { $match: { ActorId: { $ne: 1 }, State: 0 } },
-      { $addFields: { len: { $size: "$Sells" }}},
-      { $sort: { len: -1 }},
-      { $skip: request.pageindex * 5 },
-      { $limit: 5 }
-    ]);
-    
-    totalRecords = await lookModel.countDocuments({ State: 0 });
-    
-    let leaderboardArray = [ ];    
-    
-    for (let look of looks) {
-      const user = await userModel.findOne({ ActorId: look.ActorId });
-      
-      lookArray.push({
-        LookId: look.LookId,
-        ActorId: look.ActorId,
-        Created: formatDate(look.Created),
-        Headline: look.Headline,
-        LookData: look.LookData,
-        Likes: look.Likes.length,
-        Sells: look.Sells.length,
-        ActorName: user.Name
-      });
-    };
   };
   
   return buildXML("GetHighscoreLook", {
-    totalRecords: totalRecords,
+    totalRecords: lookData.totalCount,
     pageindex: request.pageindex,
     pagesize: 5,
     items: {
