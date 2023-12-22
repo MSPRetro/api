3ad86ed99b5e1dc37815e438fa51d622e9a48f5e
Glitch (mspretro-api) <none>
2023-12-22 18:34:38 +0000

    DeleteUser2 admin check fixed

--- Services/DeleteUser2.js
+++ Services/DeleteUser2.js
@@ -5,14 +5,14 @@ const { deleteValue } = require("../Utils/Globals.js");
 exports.data = {
   SOAPAction: "DeleteUser2",
   needTicket: true,
-  levelModerator: 3
+  levelModerator: 0
 }
 
 exports.run = async (request, ActorId) => {
   let user;
   
   if (request.userIdTobeDeleted == ActorId) {
-    user = await userModel.findOne({ ActorId: ActorId });
+    user = await userModel.findOne({ ActorId: ActorId, "Extra.IsExtra": 0 });
     
     await userModel.updateOne({ ActorId: ActorId }, { $set: {
       Name: "Deleted User",
@@ -20,8 +20,8 @@ exports.run = async (request, ActorId) => {
       "Extra.IsExtra": 1
     } });
   } else {
-    user = await userModel.findOne({ ActorId: request.userIdTobeDeleted });
-    if (!user || await isModerator(user.ActorId, user, 1) || await !isModerator(ActorId, false, 3)) return buildXML("DeleteUser2", 0);
+    user = await userModel.findOne({ ActorId: request.userIdTobeDeleted, "Extra.IsExtra": 0 });
+    if (!user || await isModerator(user.ActorId, user, 1) || !await isModerator(ActorId, false, 3)) return buildXML("DeleteUser2", 0);
     
     await userModel.updateOne({ ActorId: user.ActorId }, { $set: {
       Name: "Deleted User",
