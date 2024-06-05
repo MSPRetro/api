1056ed22ff09193cc513d3d648a59eb1f5a08583
Glitch (mspretro-api) <none>
2024-06-05 09:27:50 +0000

    FindUser2 research improved

--- Services/FindUser2.js
+++ Services/FindUser2.js
@@ -7,41 +7,19 @@ exports.data = {
   levelModerator: 0
 };
 
-exports.run = async (request, ActorId) => {
-  // const users = await userModel.find({ Name: request.searchString })
-  // .collation({ locale: "en", strength: 2 })
-  // .skip(request.pageindex * 10)
-  // .limit(10);
-  
-  // const users = await userModel.find({ Name: { $regex : new RegExp(request.searchString, "i") } })
-  // .skip(request.pageindex * 10)
-  // .limit(10);
-  
-  const users = await userModel.aggregate([
-    { $match: {
-      $expr: {
-        $gt: [{ $indexOfCP: [ "$Name", request.searchString.toString() ] }, -1]
-      }}
-    }
-  ])
+exports.run = async (request, ActorId) => {  
+  const users = await userModel.find({ Name: request.searchString.toString().trim() })
   .skip(request.pageindex * 10)
-  .limit(10);
+  .limit(10)
+  .collation({ locale: "en", strength: 2 });
   
   let ActorName = [ ];
   for (let user of users) {
     ActorName.push({ ActorId: user.ActorId, Name: user.Name });
   };
   
-  const totalRecords = await userModel.aggregate([
-    { $match: {
-      $expr: {
-        $gt: [{ $indexOfCP: [ "$Name", request.searchString.toString() ] }, -1]
-      }}
-    }
-  ]);
-    
   return buildXML("FindUser2", {
-    totalRecords: totalRecords.length,
+    totalRecords: await userModel.countDocuments({ Name: request.searchString.toString().trim() }),
     pageindex: request.pageindex,
     pagesize: 10,
     items: {
