b5a5bbd01565f690d9a8419d341642f366cf83ef
Glitch (mspretro-api) <none>
2024-06-23 18:35:45 +0000

    CORS fixed for payments, some packages updated

--- Services/FindUser2.js
+++ Services/FindUser2.js
@@ -7,20 +7,20 @@ exports.data = {
   levelModerator: 0
 };
 
-exports.run = async (request, ActorId) => {  
-  const users = await userModel.find({ Name: { $regex: new RegExp(`^${request.searchString.toString().trim()}`, "i") } })
+exports.run = async (request, ActorId) => {
+  const username = request.searchString.toString().trim();
+  
+  const users = await userModel.find({ Name: { $regex: new RegExp(`^${username}`, "i") } })
   .skip(request.pageindex * 10)
   .limit(10)
-  .collation({ locale: 'en', strength: 2 });
-
+  .collation({ locale: "en", strength: 2 });
   
   let ActorName = [ ];
-  for (let user of users) {
-    ActorName.push({ ActorId: user.ActorId, Name: user.Name });
-  };
+  
+  for (let user of users) ActorName.push({ ActorId: user.ActorId, Name: user.Name });
   
   return buildXML("FindUser2", {
-    totalRecords: await userModel.countDocuments({ Name: { $regex: new RegExp(`^${request.searchString.toString().trim()}`, "i") } }),
+    totalRecords: await userModel.countDocuments({ Name: { $regex: new RegExp(`^${username}`, "i") } }),
     pageindex: request.pageindex,
     pagesize: 10,
     items: {
